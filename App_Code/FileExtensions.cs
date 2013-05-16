d1aca576ffbcbb291e028e55f1e431fd2a9cf52f
mikebrind <mike@mikebrind.com>
2013-05-16 08:44:30 +0100

    First commit

---
+++ App_Code/FileExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class FileExtensions
+{
+    public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo directoryInfo, params string[] extensions) {
+        var allowedExtensions = new HashSet<string>(extensions, StringComparer.OrdinalIgnoreCase);
+        return directoryInfo.EnumerateFiles().Where(f => allowedExtensions.Contains(f.Extension));
+    } 
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+CKEDitorFileBrowser
+===================
+Code to support article on integrating a custom ASP.NET Razor Web Pages file browser with CKEditor
+
+http://www.mikesdotnetting.com/Article/212/A-CKEditor-File-Browser-For-ASP.NET-Web-Pages
